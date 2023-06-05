ce2ece053e181c06c5c6a9012ac8cfe27a452c3b
hominlee0520 <bup0520f@gmail.com>
2023-06-05 12:55:03 +0900

    app.js

--- CafeFinder_RN/App.js
+++ CafeFinder_RN/App.js
@@ -1,25 +1,12 @@
 // TEST입니다 TEST에요~
-import {
-  StyleSheet,
-  ScrollView,
-  Button,
-  View,
-  Text,
-<<<<<<< HEAD
-  TextInput,
-} from "react-native";
-import Search from "./component/Search";
-import DBTest from "./component/DBTest";
-=======
-  TextInput
-  } from 'react-native'
+import { View } from "react-native";
 
-import Search from './component/Search'
->>>>>>> dd7edf78de680ce98d245f8caea28f163d9ad568
+import Search from "./component/Search";
+
 export default function App() {
   return (
     <View>
-      <DBTest />
+      <Search />
     </View>
   );
 }
