fb3017f984e5c04651dc0ec64cf2f7a19c274bdc
민혁\als39 <minhyuk3954@gmail.com>
2023-06-05 21:19:47 +0900

    retry

--- CafeFinder_RN/App.js
+++ CafeFinder_RN/App.js
@@ -1,11 +1,13 @@
 // TEST입니다 TEST에요~~
 import { View } from "react-native";
 import Search from "./component/Search";
+import { NavigationContainer } from '@react-navigation/native';
+import Main from './component/StackNavigator'
 
 export default function App() {
   return (
     <View>
-      <Search />
+      <Main/>
     </View>
   );
 }
