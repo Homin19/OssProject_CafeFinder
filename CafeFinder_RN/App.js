cd1a5f7ba62de7a532bde3d1f9cf0c7cf54cb177
JaeYeon/DestTop <wasid4598@naver.com>
2023-06-17 21:02:44 +0900

    app.js condensation

--- CafeFinder_RN/App.js
+++ CafeFinder_RN/App.js
@@ -1,30 +1,15 @@
-/* import { NavigationContainer } from "@react-navigation/native";
-import StackNavi from "./navigator/StackNavi";
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <StackNavi />
-    </NavigationContainer>
-  );
-}*/
-// 오류나는부분 -> 검색창 asyncstorage 계속 뜸 수정하라는대로 다 수정했는데도 안돼..
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import SearchScreen from "./Screen/searchScreen";
-import Result from "./Screen/Result";
+import StackNavi from './navigator/StackNavi'
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Main" component={SearchScreen} />
-        <Stack.Screen name="Result" component={Result} />
-      </Stack.Navigator>
+      <StackNavi />
     </NavigationContainer>
-  );
+  )
 }
 
 export default App;
